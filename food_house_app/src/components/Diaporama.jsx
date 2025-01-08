254dfe7d0a35a2040e754b41c1ea6ca57c6810d9
SALL Oumar <119567549+oumar146@users.noreply.github.com>
2025-01-08 19:57:32 +0100

    Amélioration de l'interface
    ajout de classes  css

--- food_house_app/src/components/Diaporama.jsx
+++ food_house_app/src/components/Diaporama.jsx
@@ -22,13 +22,13 @@ const Diaporama = () => {
       className="mySwiper"
     >
       <SwiperSlide>
-        <img src="https://pizzeria-vendeuvre.fr/wp-content/uploads/2022/04/header-pizzas.jpg" className='item' />
+        <img src="https://pizzeria-vendeuvre.fr/wp-content/uploads/2022/04/header-pizzas.jpg"  alt="pizza" className='item' />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="https://pizzeria-vendeuvre.fr/wp-content/uploads/2022/04/PJ2202-0099-Lucasson-Frederic-header-boutique.jpg" className='item' />
+        <img src="https://pizzeria-vendeuvre.fr/wp-content/uploads/2022/04/PJ2202-0099-Lucasson-Frederic-header-boutique.jpg" alt="pizza" className='item' />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="https://zoneboreale.com/app/uploads/2024/10/fromagerie-st-laurent.jpg" className='item' />
+        <img src="https://zoneboreale.com/app/uploads/2024/10/fromagerie-st-laurent.jpg" alt="pizza" className='item' />
       </SwiperSlide>
     </Swiper>
   );
